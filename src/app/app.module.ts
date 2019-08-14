4e3eff57969fb3de716c592d2396b1501a5509fc
Akhilesh <AkhileshM@outlook.in>
2019-08-14 19:18:02 +0530

    Imported csvService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CsvService } from 'angular2-json2csv';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [CsvService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
